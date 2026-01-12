d6b2693e089105464447cf97bd100570a20e8a2f
NguyenHoa22643291 <22643291.hoa@student.iuh.edu.vn>
2026-01-12 23:42:20 +0700

    minh chung chat/read message

---
+++ src/test/java/com/example/demo/NgthiMyHoa22643291Lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgthiMyHoa22643291Lab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
